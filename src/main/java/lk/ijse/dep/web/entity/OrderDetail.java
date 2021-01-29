f692025e1748b048a8814d566c85a0ab1bdb9a1a
Uvin Weththasinghe <uvin6667@gmail.com>
2021-01-29 23:11:32 +0530

    Apply ORM to entities

--- src/main/java/lk/ijse/dep/web/entity/OrderDetail.java
+++ src/main/java/lk/ijse/dep/web/entity/OrderDetail.java
@@ -1,22 +1,30 @@
 package lk.ijse.dep.web.entity;
 
-import java.io.Serializable;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
 import java.math.BigDecimal;
 
+@Entity
+@Table(name = "order_detail")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class OrderDetail implements SuperEntity {
 
+    @EmbeddedId
     private OrderDetailPK orderDetailPK;
     private int qty;
+    @Column(name = "unit_price")
     private BigDecimal unitPrice;
-
-    public OrderDetail() {
-    }
-
-    public OrderDetail(OrderDetailPK orderDetailPK, int qty, BigDecimal unitPrice) {
-        this.orderDetailPK = orderDetailPK;
-        this.qty = qty;
-        this.unitPrice = unitPrice;
-    }
+    @ManyToOne
+    @JoinColumn(name="order_id", referencedColumnName = "id", insertable = false, updatable = false)
+    private Order order;
+    @ManyToOne
+    @JoinColumn(name="item_code", referencedColumnName = "code", insertable = false, updatable = false)
+    private Item item;
 
     public OrderDetail(String orderId, String itemCode, int qty, BigDecimal unitPrice) {
         this.orderDetailPK = new OrderDetailPK(orderId, itemCode);
@@ -24,36 +32,9 @@ public class OrderDetail implements SuperEntity {
         this.unitPrice = unitPrice;
     }
 
-    public OrderDetailPK getOrderDetailPK() {
-        return orderDetailPK;
-    }
-
-    public void setOrderDetailPK(OrderDetailPK orderDetailPK) {
+    public OrderDetail(OrderDetailPK orderDetailPK, int qty, BigDecimal unitPrice) {
         this.orderDetailPK = orderDetailPK;
-    }
-
-    public int getQty() {
-        return qty;
-    }
-
-    public void setQty(int qty) {
         this.qty = qty;
-    }
-
-    public BigDecimal getUnitPrice() {
-        return unitPrice;
-    }
-
-    public void setUnitPrice(BigDecimal unitPrice) {
         this.unitPrice = unitPrice;
-    }
-
-    @Override
-    public String toString() {
-        return "OrderDetail{" +
-                "orderDetailPK=" + orderDetailPK +
-                ", qty=" + qty +
-                ", unitPrice=" + unitPrice +
-                '}';
     }
 }
